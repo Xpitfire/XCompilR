ca7bfc412c37fc1800044916b49d27d12bb807e8
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-01-06 01:03:35 +0100

    Added binding classes.

---
+++ XCompilR/XCompilR.ParserGen.Library/ABindingParserGen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XCompilR.ParserGen.Library
+{
+    [Serializable]
+    public abstract class ABindingParserGen
+    {
+        public abstract string ParserName { get; }
+        public abstract string AssemblyName { get; }
+        public abstract IParserGen Parser { get; }
+    }
+}
---
+++ XCompilR/XCompile.ParserGen.CocoR/BindingParserGen.cs
@@ -0,0 +1,16 @@
+﻿using XCompilR.ParserGen.Library;
+
+namespace XCompile.ParserGen.CocoR
+{
+    public class BindingParserGen : ABindingParserGen
+    {
+        public override string ParserName { get; } = "CocoRParserGen";
+        public override string AssemblyName { get; } = "XCompilR.ParserGen.CocoR";
+        public override IParserGen Parser { get; }
+
+        public BindingParserGen()
+        {
+            Parser = new CocoParserGen();
+        }
+    }
+}
